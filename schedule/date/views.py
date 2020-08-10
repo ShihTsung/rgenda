1a4f896b1786ad847cd012021a3d57783017b287
circlepen <b01505008@ntu.edu.tw>
2020-08-10 16:42:17 +0800

    total-per-day api and ajusment model change

--- schedule/date/views.py
+++ schedule/date/views.py
@@ -10,6 +10,12 @@ def date_manage(request):
     return render(request, 'calendars/date_manage.html', context)
 
 
+@login_required
+def config_manage(request):
+    context = {'LANG': request.LANGUAGE_CODE}
+    return render(request, 'calendars/config_manage.html', context)
+
+
 def attr_list(start, end):
     """
     取得醫院行事曆類別，分為workday(一般工作日)，holiday(休息工作日)，closed_day(休診日)
