e72b7f2924b99cbce6fb7bb889ac9775febdb976
Sophie Su <sophie.su@redfalcon-hpc.com>
2020-08-11 13:59:46 +0800

    Fix naming style

--- schedule/resources/src/navbar/notice-list.js
+++ schedule/resources/src/navbar/notice-list.js
@@ -1,11 +1,11 @@
 import Vue from 'vue';
-import NoticeList from 'components/navbar/noticeList.vue';
+import NoticeList from 'components/navbar/NoticeList.vue';
 import HttpClient from 'common/http';
 Vue.use(HttpClient);
 
 new Vue({
   el: '#navbar',
   components: {
-    'notice-list': NoticeList,
+    NoticeList,
   },
 });
