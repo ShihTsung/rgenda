d7cca490e1fe3bb2d0c4e15b94eb90578f4aed82
circlepen <b01505008@ntu.edu.tw>
2020-08-20 10:00:00 +0800

    fix getexchangeapplication api results

--- schedule/api/serializers.py
+++ schedule/api/serializers.py
@@ -507,7 +507,12 @@ class GetExchangeApplicationSerializer(serializers.ModelSerializer):
         date = obj.date_start
         try:
             ret = Result.objects.get(user=obj.user_apply, date=date)
-            ret = GetResultSerializer(ret)
+            ret = {
+                'user': {'id': ret.user.id, 'name': ret.user.full_name},
+                'date': ret.date.strftime('%Y-%m-%d'),
+                'shift': {'id': ret.shift.id, 'name': ret.shift.name},
+                'station': {'id': ret.station.id, 'name': ret.station.name}
+            }
         except Result.DoesNotExist:
             ret = ''
         return ret
@@ -517,7 +522,12 @@ class GetExchangeApplicationSerializer(serializers.ModelSerializer):
         date = obj.date_start
         try:
             ret = Result.objects.get(user=obj.user_receive, date=date)
-            ret = GetResultSerializer(ret)
+            ret = {
+                'user': {'id': ret.user.id, 'name': ret.user.full_name},
+                'date': ret.date.strftime('%Y-%m-%d'),
+                'shift': {'id': ret.shift.id, 'name': ret.shift.name},
+                'station': {'id': ret.station.id, 'name': ret.station.name}
+            }
         except Result.DoesNotExist:
             ret = ''
         return ret
