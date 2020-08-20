afc21885eeaab6444fb4919b183460e27f3626d9
Sophie Su <sophie.su@redfalcon-hpc.com>
2020-08-20 09:11:06 +0800

    Fix merge bug

--- schedule/resources/components/department/DepartmentEdit.vue
+++ schedule/resources/components/department/DepartmentEdit.vue
@@ -160,7 +160,13 @@ export default {
         let department = responses[0].data;
         department.date_start = moment(department.date_start).toDate();
         department.original_managers = cloneDeep(department.managers);
-        self.department = Object.assign(self.department, department);
+        if (!department.managers.manager1) {
+          department.managers.manager1 = {};
+        }
+        if (!department.managers.manager2) {
+          department.managers.manager2 = {};
+        }
+        self.department = department;
 
         let userList = responses[1].data;
         if (Array.isArray(userList)) {
