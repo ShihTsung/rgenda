7e7c2f7be5f82758d6c45aaccfde582ac811b640
Sophie Su <sophie.su@redfalcon-hpc.com>
2020-09-09 09:24:19 +0800

    Modify .gitignore file

--- schedule/schedule/settings.py
+++ schedule/schedule/settings.py
@@ -153,7 +153,7 @@ if os.environ.get('APP_ENV') == "local":
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'DB',
+            'NAME': 'DB_TEST',
             'USER': 'circlepen',
             'PASSWORD': 'redfalcon',
             'HOST': 'mysqlDB',
@@ -165,7 +165,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'DB_TEST',
+            'NAME': 'DB',
             'USER': 'circlepen',
             'PASSWORD': 'redfalcon',
             'HOST': 'mysqlDB',
