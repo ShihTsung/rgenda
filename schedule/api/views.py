291a45457dcd59ec15b35dc134ebe540dee444d9
maxchen <max.chen@ylhealth.org>
2020-09-11 16:30:22 +0800

    create result update

--- schedule/api/views.py
+++ schedule/api/views.py
@@ -1755,7 +1755,7 @@ def recreate_result(request):
                 # for cycle 計算班表
                 for ind, cycle in enumerate(cycle_list):
 
-                    for _ in range(10000):
+                    for _ in range(1000):
 
                         # 產生需求校正list和指標
                         diff_list = [attr_dict[ind]['q'] for _ in range(attr_dict[ind]['day_num'])]
@@ -1886,7 +1886,7 @@ def recreate_result(request):
                             # 儲存結果
                             output = temp_output
                             print(station.name, shift.name, 'Level', str(demand['demand'].level), 'Cycle', str(ind),
-                                  'Success in 2000.')
+                                  'Success in 1000.')
 
                             # 儲存剩餘工作天 & 可休假假日數
                             for user_id in user_pool:
@@ -1903,7 +1903,7 @@ def recreate_result(request):
                         # 嘗試10000次皆失敗，強制產生班表，不必滿足所有需求
                         # 嘗試排班100次，取最滿足需求的結果
                         print(station.name, shift.name, 'Level', str(demand['demand'].level), 'Cycle', str(ind),
-                              'Fail in 10000, force creating.')
+                              'Fail in 1000, force creating.')
                         best_temp_output = None
                         demand_loss = attr_dict[ind]['total_demands']
 
