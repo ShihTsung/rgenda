eb17d7b23e16a047da2c1bc32a12e5f48b3373d0
maxchen <max.chen@ylhealth.org>
2020-09-17 17:08:06 +0800

    update recreate monthly, avoid on duty at close day

--- schedule/api/urls.py
+++ schedule/api/urls.py
@@ -41,5 +41,6 @@ urlpatterns = [
     path('suggest-user-num/<date_str>/', views.suggest_user_num),
     path('recreate-result-periodic', views.recreate_result_periodic),
     path('recreate-result-monthly', views.recreate_result_monthly),
+    path('recreate-result-monthly-b', views.recreate_result_monthly_b),
     path('published-or-not', views.published_or_not)
 ]
