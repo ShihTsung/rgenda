bdea13ea8f851f7729f7a19cb850a7b7eed6c3c0
Sophie Su <sophie.su@redfalcon-hpc.com>
2020-09-23 16:52:04 +0800

    Fix offHours to exclude official leave

--- schedule/resources/components/pre-result/PreResult.vue
+++ schedule/resources/components/pre-result/PreResult.vue
@@ -982,16 +982,17 @@ export default {
         // 只有當
         // 1. 當日無預排結果
         // 2. 預排結果與預約假勤不同
-        // 時才計算，不重複計算假日
+        // 時才計算，不重複計算假日，且排除公假
         if (
           item.user.id === u &&
           (
             !this.shiftOfCurrentMonth[u] ||
             !this.shiftOfCurrentMonth[u][day] ||
-            this.shiftOfCurrentMonth[u][day].shift.shift_type !== shiftType
+            this.shiftOfCurrentMonth[u][day].shift.shift_type !== shiftType &&
+            item.shift_type !== this.$getPromiseLeaveItemValue('ITEM_OFFICIAL_LEAVE')
           )
         ) {
-          if (shiftType === this.$getShiftTypeValue("VALUE_PAID_LEAVE") && item.shift_type !== this.$getPromiseLeaveItemValue('ITEM_OFFICIAL_LEAVE')) {
+          if (shiftType === this.$getShiftTypeValue("VALUE_PAID_LEAVE")) {
             ++count;
           } else {
             ++notCount;
