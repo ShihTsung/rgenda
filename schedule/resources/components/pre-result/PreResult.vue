3660b523cad197fd298c5a8483869b9ad4bf5e11
Sophie Su <sophie.su@redfalcon-hpc.com>
2020-09-23 12:29:14 +0800

    Fix noRest calculation

--- schedule/resources/components/pre-result/PreResult.vue
+++ schedule/resources/components/pre-result/PreResult.vue
@@ -958,22 +958,14 @@ export default {
         }
       }
 
-      let month = new Date().getMonth() + 1;
       this.adjustHr
-        .filter((item) => {
-          return (
-            item.user == u && (
-              parseInt(item.date.split("-")[1]) < month || (
-                parseInt(item.date.split("-")[1]) === month &&
-                parseInt(item.date.split("-")[2]) <= this.date
-              )
-            )
-          );
-        })
         .forEach((i) => {
           if (
-            this.$getTimeAdjustmentItemValue('ITEM_OFF_DAY_ATTENDANCE') === i.adjustment_item ||
-            this.$getTimeAdjustmentItemValue('ITEM_NATIONAL_HOLIDAY_ATTENDANCE') === i.adjustment_item
+            i.user == u &&
+            parseInt(i.date.split("-")[1]) === this.month && (
+              this.$getTimeAdjustmentItemValue('ITEM_OFF_DAY_ATTENDANCE') === i.adjustment_item ||
+              this.$getTimeAdjustmentItemValue('ITEM_NATIONAL_HOLIDAY_ATTENDANCE') === i.adjustment_item
+            )
           ) {
             ++noRest;
           }
