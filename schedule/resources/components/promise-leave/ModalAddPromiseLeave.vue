5530cf5a364d22e0792543ba805697b98ab566d4
Sophie Su <sophie.su@redfalcon-hpc.com>
2020-09-24 08:56:02 +0800

    Add alert text

--- schedule/resources/components/promise-leave/ModalAddPromiseLeave.vue
+++ schedule/resources/components/promise-leave/ModalAddPromiseLeave.vue
@@ -27,6 +27,9 @@
                 }'
                 :min-date="minDate"
                 ></date-picker>
+                <small class="form-text text-danger">
+                  只可選擇未發布班表的月份。若班表已發佈，請至總班表管理頁面編輯。
+                </small>
             </div>
             <div class="form-group">
               <label class="font-weight-bold">類別</label>
